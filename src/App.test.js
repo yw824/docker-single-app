1512e5d591dc25001f48a5c4734f9663baa218e6
def47619 <def47619@gmail.com>
2024-01-13 21:50:00 +0900

    initial commit

---
+++ src/App.test.js
@@ -0,0 +1,24 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// 위와 동일한 테스트 코드
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+// 위와 동일한 테스트 코드 하나 더 추가
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
